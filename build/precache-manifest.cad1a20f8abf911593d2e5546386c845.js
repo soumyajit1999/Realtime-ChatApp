dcd1d55ee4567331e3c37494b25a8e16bc23eead
Soumyajit Sen <66264486+soumyajit1999@users.noreply.github.com>
2020-09-13 17:06:40 +0530

    Add files via upload

---
+++ build/precache-manifest.cad1a20f8abf911593d2e5546386c845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4bd95a4fe575ac544a7ee8c8a876d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d13195570d1c24aa126d",
+    "url": "/static/css/main.b6ef79a9.chunk.css"
+  },
+  {
+    "revision": "0d8cfa370553c961d058",
+    "url": "/static/js/2.388b021d.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.388b021d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13195570d1c24aa126d",
+    "url": "/static/js/main.74aac580.chunk.js"
+  },
+  {
+    "revision": "ca27e559dcb301b35779",
+    "url": "/static/js/runtime-main.2bd2a3a7.js"
+  }
+]);
